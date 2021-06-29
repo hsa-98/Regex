1050d9493965c25661bf98cba6b4edfadb9701f3
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-29 14:54:15 +0530

    Can enter email id

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,30 +4,35 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+
         System.out.println("Enter first name");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
-        String pattern ="(^[A-Z])([A-Za-z]{2,})";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(name);
-        Boolean match = m.matches();
-        if(match){
+        String pattern ="^[A-Z][A-Za-z]{2,}";
+        userRegistration.validate(pattern,name);
+
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        pattern = "^[A-Z][a-z]{2,}";
+        userRegistration.validate(pattern,lastName);
+
+        System.out.println("Enter email id");
+        String email = sc.next();
+        pattern = "^[a-z]{3}[a-zA-Z0-9+._-]*@[a-z]{2}[.]{1}[a-z]{2}[.]*[a-z]*$";
+        userRegistration.validate(pattern,email);
+
+
+    }
+    public void  validate(String pattern,String data){
+        Pattern pattern1 = Pattern.compile(pattern);
+        Matcher m = pattern1.matcher(data);
+        Boolean matches = m.matches();
+        if(matches){
             System.out.println("It is valid");
         }
         else{
             System.out.println("Input is not valid");
-        }
-        System.out.println("Enter last name");
-        String LastName = sc.next();
-        pattern = "(^[A-Z])([a-z]){2,}";
-        r= Pattern.compile(pattern);
-        m=r.matcher(LastName);
-        match= m.matches();
-        if(match){
-            System.out.println("Last name is valid");
-        }
-        else{
-            System.out.println("Last name is not valid");
         }
     }
 }
